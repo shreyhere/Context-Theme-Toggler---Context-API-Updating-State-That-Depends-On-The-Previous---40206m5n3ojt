1dce68483749e2f6086cca629b7ef01102219716
shreyhere <116891326+shreyhere@users.noreply.github.com>
2023-01-06 19:54:46 +0530

    Update ThemeToggleButton.js

--- src/components/ThemeToggleButton.js
+++ src/components/ThemeToggleButton.js
@@ -1,14 +1,29 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
+import { ThemeContext } from './ThemeProvider';
 
-const ThemeContext = React.createContext()
-const ThemeProvider = (props) =>{
-     const [theme, setTheme] = useState('light')
+const ThemeToggleButton = () =>{
+    
+    const [toggle,setToggle] = useState('Switch to dark theme');
+    //const [theme1,setTheme1] = useState('light');
+
+    const {theme,setTheme} = useContext(ThemeContext);
+    // console.log(theme);
+
+    let global = () =>{
+
+        if(toggle === 'Switch to dark theme'){
+            setTheme('dark')
+            setToggle('Switch to light theme');
+        }else{
+            setTheme('light');
+            setToggle('Switch to dark theme');
+        }
+    }
     return (
-        <React.Fragment>
-            <ThemeContext.Provider value={{theme, setTheme}}>
-                {props.children}
-            </ThemeContext.Provider>
-        </React.Fragment>
+       <>
+       <button className={`btn btn-${theme} txt-${theme}`} id='global-theme-toggler' onClick={global}>{toggle}</button>
+       </>
     )
+
 }
-export {ThemeProvider,ThemeContext}
+export {ThemeToggleButton}
